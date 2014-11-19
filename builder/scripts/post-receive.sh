3feacbc15e456c2fe6131b57b2ce9b4fcb54dcba
Rizo Isrof <rizo@odis.io>
2014-11-19 16:11:44 +0000

    Make

---
+++ builder/scripts/post-receive.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pwd > /User/rizo/Desktop/xx.log
+
+# echo "[`date +"%Y-%m-%d %H:%M:%S"`] Building and exporting the D SDK..."
+# make export
+# echo "---\n"
+
+
+# make -C $GIT_DIR/.. export | tee -a $GIT_DIR/../builds.log >&- 2>&- & 
