20d95540f9f201070a345cf4bb73981b04c0a48c
Rizo Isrof <rizo@odis.io>
2014-11-19 15:29:01 +0000

    Fixed dir.

--- builder/scripts/post-receive.sh
+++ builder/scripts/post-receive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "[`date +"%Y-%m-%d %H:%M:%S"`] Building and exporting the D SDK..." >> $ROOT/builds.log
-make -C $ROOT export-builds >> $ROOT/builds.log
-echo "---\n" >> $ROOT/builds.log
+make -C $REPOSITORY_DIR export-builds >> $REPOSITORY_DIR/builds.log
+echo "---\n" >> $REPOSITORY_DIR/builds.log
